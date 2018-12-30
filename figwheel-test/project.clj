cb45c0ed5771d5a0156a19104fe821576de7a86b
Nick (Git client) <nick@cassiel.com>
2018-12-30 19:11:12 +0000

    Figwheel experiments.

--- figwheel-test/project.clj
+++ figwheel-test/project.clj
@@ -1,18 +1,22 @@
-(defproject first-shot "0.1.0-SNAPSHOT"
+(defproject figwheel-test "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/clojurescript "1.9.521"]]
-  :plugins [[lein-cljsbuild "1.1.5"]]
+  :plugins [[lein-cljsbuild "1.1.5"]
+            [lein-figwheel "0.5.18"]]
+  :clean-targets ^{:protect false} ["target"]
   :cljsbuild {:builds [{:id "prod"
                         :source-paths ["src-cljs"]
-                        :compiler {:main net.cassiel.first-shot.core
+                        :figwheel true
+                        :compiler {:main net.cassiel.figwheel-test.core
                                    :output-to "package/index.js"
                                    :target :nodejs
                                    :output-dir "target"
                                    ;; :externs ["externs.js"]
-                                   :optimizations :advanced
+                                   :optimizations :none
                                    :pretty-print true
-                                   :parallel-build true}}]})
+                                   :parallel-build true}}]}
+  :figwheel {})
