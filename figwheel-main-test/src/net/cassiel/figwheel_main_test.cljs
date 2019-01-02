89d3fbc68162ec46fbee6688548c5a50ff11892d
Nick (Git client) <nick@cassiel.com>
2019-01-02 21:45:47 +0000

    figwheel-main example.

---
+++ figwheel-main-test/src/net/cassiel/figwheel_main_test.cljs
@@ -0,0 +1,23 @@
+(ns ^:figwheel-hooks net.cassiel.figwheel-main-test
+  (:require
+   [goog.dom :as gdom]))
+
+(println "This text is printed from src/net/cassiel/figwheel_main_test.cljs. Go ahead and edit it and see reloading in action.")
+
+(defn multiply [a b] (* a b))
+
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:text "Hello world!"}))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+
+
+;; specify reload hook with ^;after-load metadata
+(defn ^:after-load on-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
