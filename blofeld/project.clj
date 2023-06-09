dbd8be532e7ea8955504d89394db0d3cf7e265e1
Nick (Git client) <nick@cassiel.com>
2023-06-09 12:51:08 +0100

    Version bumps.

--- blofeld/project.clj
+++ blofeld/project.clj
@@ -1,17 +1,18 @@
 (defproject promises "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.758"]
-                                  [org.clojure/core.async "1.3.610"]
-                                  [org.clojure/core.match "1.0.0"]
-                                  [com.stuartsierra/component "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.11.60"]
+                                  [org.clojure/core.async "1.6.673"]
+                                  [org.clojure/core.match "1.0.1"]
+                                  [com.stuartsierra/component "1.1.0"]
                                   [net.cassiel/lifecycle "0.1.0-SNAPSHOT"]
-                                  [com.bhauman/figwheel-main "0.2.3"]
+                                  [com.bhauman/figwheel-main "0.2.18"]
                                   ;; optional but recommended
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]
                                   ;; NICK: add support for resolving Javascript promises:
                                   [jamesmacaulay/cljs-promises "0.1.0"]
                                   ;; Other useful stuff:
-                                  [binaryage/oops "0.6.4"]]
+                                  [binaryage/oops "0.7.2"]]
                    :resource-paths ["target"]
                    :clean-targets ^{:protect false} ["target"]}}
+  :plugins [[com.github.liquidz/antq "RELEASE"]]
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
--- simple/project.clj
+++ simple/project.clj
@@ -1,9 +1,10 @@
 (defproject simple "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.10.339"]
-                                  [com.bhauman/figwheel-main "0.2.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.11.60"]
+                                  [com.bhauman/figwheel-main "0.2.18"]
                                   ;; optional but recommended
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    :resource-paths ["target"]
                    :clean-targets ^{:protect false} ["target"]}}
+  :plugins [[com.github.liquidz/antq "RELEASE"]]
   :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]})
